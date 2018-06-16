dc69bafa195a7d87567384defd79dfc335f9ca52
Matthias <matthias.bungeroth@tum.de>
2018-06-16 23:57:01 +0200

    added Bot

--- Drone/src/main/java/Main.java
+++ Drone/src/main/java/Main.java
@@ -8,7 +8,18 @@ public class Main {
 
         Thread t = new SendThread(InetAddress.getByName("172.16.10.1"));
         t.start();
-        Thread.sleep(10000);
 
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+        try {
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+
+        Thread.sleep(10000);
+        System.exit(0);
     }
 }
---
+++ Drone/src/main/java/WlanNameFinder.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class WlanNameFinder {
+
+
+    private static final String[] keywords ={"drone", "9300"};
+
+    public static void main(String []aghs) throws IOException {
+
+        getNames();
+
+
+    }
+
+
+
+    //
+    public static Set<String> getNames() throws IOException {
+
+        Runtime rt = Runtime.getRuntime();
+
+
+        Process proc = rt.exec("nmcli dev wifi");
+
+        Set<String> resutl = new HashSet<String>(20);
+
+        BufferedReader stdInput = new BufferedReader(new
+                InputStreamReader(proc.getInputStream()));
+        stdInput.readLine();
+
+
+        String s = null;
+        while ((s = stdInput.readLine()) != null) {
+            String[] arr = s.replace("*" , "").trim().replaceAll(" +", " ").split(" ");
+
+            //This means no SEC
+            if(arr.length == 8 && Stream.of(keywords).map( st -> arr[0].contains(st)).parallel().anyMatch(b-> b)){
+                resutl.add(arr[0]);
+                System.out.println(s);
+            }
+
+        }
+
+
+        return resutl;
+    }
+}
